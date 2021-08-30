4e895c69e6b01a4ca59e540f6b0bca6db0313547
Lovish Hamal <lovishhamal123@gmail.com>
2021-08-30 22:31:05 +0545

    feat: added async popup model for entity delete

--- Resources/assets/Component/TableComponent/TableComponent.tsx
+++ Resources/assets/Component/TableComponent/TableComponent.tsx
@@ -2,215 +2,285 @@
  * @copyright EveryWorkflow. All rights reserved.
  */
 
-import React, {useCallback, useContext, useState} from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import React, { useCallback, useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Table from 'antd/lib/table';
 import Menu from 'antd/lib/menu';
 import Dropdown from 'antd/lib/dropdown';
 import Button from 'antd/lib/button';
+import Popconfirm from 'antd/lib/popconfirm';
+import message from 'antd/lib/message';
 import DataGridContext from '@EveryWorkflow/DataGridBundle/Context/DataGridContext';
 import FilterComponent from '@EveryWorkflow/DataGridBundle/Component/FilterComponent';
 import HeaderPanelComponent from '@EveryWorkflow/DataGridBundle/Component/HeaderPanelComponent';
-import {DATA_GRID_TYPE_INLINE} from '@EveryWorkflow/DataGridBundle/Component/DataGridComponent/DataGridComponent';
+import { DATA_GRID_TYPE_INLINE } from '@EveryWorkflow/DataGridBundle/Component/DataGridComponent/DataGridComponent';
 import ColumnConfigComponent from '@EveryWorkflow/DataGridBundle/Component/ColumnConfigComponent';
-import {ACTION_SET_SELECTED_ROW_IDS} from '@EveryWorkflow/DataGridBundle/Reducer/DataGridReducer';
+import { ACTION_SET_SELECTED_ROW_IDS } from '@EveryWorkflow/DataGridBundle/Reducer/DataGridReducer';
 import EllipsisOutlined from '@ant-design/icons/EllipsisOutlined';
-import SelectFieldInterface from "@EveryWorkflow/DataFormBundle/Model/Field/SelectFieldInterface";
+import SelectFieldInterface from '@EveryWorkflow/DataFormBundle/Model/Field/SelectFieldInterface';
 
+let id: string = '';
 const TableComponent = () => {
-    const {state: gridState, dispatch: gridDispatch} = useContext(DataGridContext);
-    const [selectedRows, setSelectedRows] = useState<Array<any>>([]);
-    const history = useHistory();
-    const urlParams = new URLSearchParams(history.location.search);
+  const { state: gridState, dispatch: gridDispatch } =
+    useContext(DataGridContext);
+  const [selectedRows, setSelectedRows] = useState<Array<any>>([]);
+  const history = useHistory();
+  const urlParams = new URLSearchParams(history.location.search);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
-    const getColumnData = useCallback(() => {
-        const columnData: Array<any> = [];
-        const sortField = urlParams.get('sort-field');
-        let sortOrder = 'ascend';
-        if (urlParams.get('sort-order') && urlParams.get('sort-order') === 'desc') {
-            sortOrder = 'descend';
-        }
+  const showPopconfirm = (_id: string) => {
+    id = _id;
+    setVisible(true);
+  };
 
-        gridState.data_grid_column_state.forEach((col) => {
-            const field = gridState.data_form?.fields.find(
-                (item) => item.name === col.name
-            );
-            if (field) {
-                columnData.push({
-                    title: field.label,
-                    dataIndex: field.name,
-                    sorter: gridState.data_grid_config?.sortable_columns?.includes(field.name ?? ''),
-                    // eslint-disable-next-line react/display-name
-                    render: (value: any) => <span>{value}</span>,
-                    sortOrder: field.name === sortField ? sortOrder : false,
-                    width: 240,
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleAction = ({
+    action,
+    index,
+    record,
+  }: {
+    action: any;
+    index: number;
+    record: any;
+  }) => {
+    switch (action.label.toLowerCase()) {
+      case 'edit':
+        return (
+          <Link
+            to={() => {
+              if (action.path) {
+                let path: string = action.path;
+                Object.keys(record).forEach((itemKey: string) => {
+                  path = path.replace('{' + itemKey + '}', record[itemKey]);
                 });
-            }
-        });
-        if (columnData.length) {
-            columnData.push({
-                title: 'Action',
-                key: 'operation',
-                fixed: 'right',
-                width: 84,
-                // eslint-disable-next-line react/display-name
-                render: (_: any, record: any) => (
-                    <Dropdown
-                        overlay={
-                            <Menu>
-                                {gridState.data_grid_config?.row_actions?.map(
-                                    (action: any, index: number) => (
-                                        <Menu.Item key={index}>
-                                            <Link
-                                                to={() => {
-                                                    if (action.path) {
-                                                        let path: string = action.path;
-                                                        Object.keys(record).forEach((itemKey: string) => {
-                                                            path = path.replace(
-                                                                '{' + itemKey + '}',
-                                                                record[itemKey]
-                                                            );
-                                                        });
-                                                        return {pathname: path};
-                                                    }
-                                                    return '';
-                                                }}
-                                                target={action.button_target}
-                                            >
-                                                {action.label}
-                                            </Link>
-                                        </Menu.Item>
-                                    )
-                                )}
-                            </Menu>
-                        }
-                        trigger={['click']}
-                        placement="bottomRight"
-                    >
-                        <Button type="text" size="small">
-                            <EllipsisOutlined/>
-                        </Button>
-                    </Dropdown>
-                ),
-            });
-        }
-        return columnData;
-    }, [gridState]);
+                return path;
+              }
+              return '';
+            }}
+          >
+            {action.label}
+          </Link>
+        );
+      case 'delete':
+        return (
+          <div onClick={() => showPopconfirm(record._id)}>{action.label}</div>
+        );
+      default:
+        break;
+    }
+  };
 
-    const getDataSource = useCallback(() => {
-        const data: Array<any> = [];
-
-        const getSelectOptionValue = (field: SelectFieldInterface, fieldValue: string): string => {
-            field.options?.forEach(option => {
-                if (option.key?.toString() === fieldValue.toString()) {
-                    fieldValue = option.value?.toString();
-                }
-            });
-            return fieldValue;
-        }
-
-        gridState.data_collection?.results.forEach((item) => {
-            const newItem: any = {
-                key: item._id,
-            };
-            gridState.data_grid_column_state.forEach((col) => {
-                if (col.name in item) {
-                    const fieldValue: any = item[col.name];
-                    const field = gridState.data_form?.fields.find(field => {
-                        return field.name === col.name;
-                    });
-                    if (field && 'select_field' === field.field_type) {
-                        newItem[col.name] = getSelectOptionValue(field, fieldValue);
-                    } else {
-                        newItem[col.name] = fieldValue;
-                    }
-                }
-            });
-            data.push(newItem);
-        });
-        return data;
-    }, [gridState]);
-
-    const getRowSelection = useCallback(() => {
-        const onSelectChange = (selectedRowKeys: Array<any>) => {
-            console.log('selectedRowKeys changed: ', selectedRowKeys);
-            setSelectedRows(selectedRowKeys);
-            gridDispatch({
-                type: ACTION_SET_SELECTED_ROW_IDS,
-                payload: selectedRowKeys,
-            });
-        };
-
-        return {
-            selectedRows,
-            onChange: onSelectChange,
-            selections: [
-                Table.SELECTION_ALL,
-                Table.SELECTION_INVERT,
-                Table.SELECTION_NONE,
-            ],
-        };
-    }, [selectedRows, gridDispatch]);
-
-    const onTableChange = (pagination: any, filters: any, sorter: any) => {
-        if (gridState.data_grid_url) {
-            let newUrlPath = history.location.pathname;
-            newUrlPath += '?page=' + pagination.current;
-            if (pagination.pageSize && pagination.pageSize > 0 && pagination.pageSize !== 20) {
-                newUrlPath += '&per-page=' + pagination.pageSize;
-            }
-            if (urlParams.get('filter')) {
-                try {
-                    newUrlPath += '&filter=' + JSON.stringify(JSON.parse(urlParams.get('filter') ?? '{}'));
-                } catch (e) {
-                    // ignore urlPramData is cannot parse as json
-                }
-            }
-            if (sorter.field && sorter.order) {
-                let sortOrder = 'asc';
-                if (sorter.order === 'descend') {
-                    sortOrder = 'desc';
-                }
-                newUrlPath += '&sort-field=' + sorter.field + '&sort-order=' + sortOrder;
-            }
-            history.push(newUrlPath);
-        }
+  const getColumnData = useCallback(() => {
+    const columnData: Array<any> = [];
+    const sortField = urlParams.get('sort-field');
+    let sortOrder = 'ascend';
+    if (urlParams.get('sort-order') && urlParams.get('sort-order') === 'desc') {
+      sortOrder = 'descend';
     }
 
-    return (
+    gridState.data_grid_column_state.forEach((col) => {
+      const field = gridState.data_form?.fields.find(
+        (item) => item.name === col.name
+      );
+      if (field) {
+        columnData.push({
+          title: field.label,
+          dataIndex: field.name,
+          sorter: gridState.data_grid_config?.sortable_columns?.includes(
+            field.name ?? ''
+          ),
+          // eslint-disable-next-line react/display-name
+          render: (value: any) => <span>{value}</span>,
+          sortOrder: field.name === sortField ? sortOrder : false,
+          width: 240,
+        });
+      }
+    });
+    if (columnData.length) {
+      columnData.push({
+        title: 'Action',
+        key: 'operation',
+        fixed: 'right',
+        width: 84,
+        // eslint-disable-next-line react/display-name
+        render: (_: any, record: any) => {
+          return (
+            <>
+              {id === record._id && (
+                <Popconfirm
+                  title={'Are you sure you want delete this ?'}
+                  okText="Delete"
+                  visible={visible}
+                  onConfirm={handleOk}
+                  okButtonProps={{ loading: confirmLoading }}
+                  onCancel={handleCancel}
+                />
+              )}
+              <Dropdown
+                overlay={
+                  <Menu>
+                    {gridState.data_grid_config?.row_actions?.map(
+                      (action: any, index: number) => (
+                        <Menu.Item key={index}>
+                          {handleAction({ action, index, record })}
+                        </Menu.Item>
+                      )
+                    )}
+                  </Menu>
+                }
+                trigger={['click']}
+                placement="bottomRight"
+              >
+                <Button type="text" size="small">
+                  <EllipsisOutlined />
+                </Button>
+              </Dropdown>
+            </>
+          );
+        },
+      });
+    }
+    return columnData;
+  }, [gridState, visible, confirmLoading]);
+
+  const getDataSource = useCallback(() => {
+    const data: Array<any> = [];
+
+    const getSelectOptionValue = (
+      field: SelectFieldInterface,
+      fieldValue: string
+    ): string => {
+      field.options?.forEach((option) => {
+        if (option.key?.toString() === fieldValue.toString()) {
+          fieldValue = option.value?.toString();
+        }
+      });
+      return fieldValue;
+    };
+
+    gridState.data_collection?.results.forEach((item) => {
+      const newItem: any = {
+        key: item._id,
+      };
+      gridState.data_grid_column_state.forEach((col) => {
+        if (col.name in item) {
+          const fieldValue: any = item[col.name];
+          const field = gridState.data_form?.fields.find((field) => {
+            return field.name === col.name;
+          });
+          if (field && 'select_field' === field.field_type) {
+            newItem[col.name] = getSelectOptionValue(field, fieldValue);
+          } else {
+            newItem[col.name] = fieldValue;
+          }
+        }
+      });
+      data.push(newItem);
+    });
+    return data;
+  }, [gridState]);
+
+  const getRowSelection = useCallback(() => {
+    const onSelectChange = (selectedRowKeys: Array<any>) => {
+      console.log('selectedRowKeys changed: ', selectedRowKeys);
+      setSelectedRows(selectedRowKeys);
+      gridDispatch({
+        type: ACTION_SET_SELECTED_ROW_IDS,
+        payload: selectedRowKeys,
+      });
+    };
+
+    return {
+      selectedRows,
+      onChange: onSelectChange,
+      selections: [
+        Table.SELECTION_ALL,
+        Table.SELECTION_INVERT,
+        Table.SELECTION_NONE,
+      ],
+    };
+  }, [selectedRows, gridDispatch]);
+
+  const onTableChange = (pagination: any, filters: any, sorter: any) => {
+    if (gridState.data_grid_url) {
+      let newUrlPath = history.location.pathname;
+      newUrlPath += '?page=' + pagination.current;
+      if (
+        pagination.pageSize &&
+        pagination.pageSize > 0 &&
+        pagination.pageSize !== 20
+      ) {
+        newUrlPath += '&per-page=' + pagination.pageSize;
+      }
+      if (urlParams.get('filter')) {
+        try {
+          newUrlPath +=
+            '&filter=' +
+            JSON.stringify(JSON.parse(urlParams.get('filter') ?? '{}'));
+        } catch (e) {
+          // ignore urlPramData is cannot parse as json
+        }
+      }
+      if (sorter.field && sorter.order) {
+        let sortOrder = 'asc';
+        if (sorter.order === 'descend') {
+          sortOrder = 'desc';
+        }
+        newUrlPath +=
+          '&sort-field=' + sorter.field + '&sort-order=' + sortOrder;
+      }
+      history.push(newUrlPath);
+    }
+  };
+
+  return (
+    <>
+      {gridState.data_grid_type === DATA_GRID_TYPE_INLINE && (
+        <HeaderPanelComponent />
+      )}
+      {gridState.data_collection && (
         <>
-            {gridState.data_grid_type === DATA_GRID_TYPE_INLINE && (
-                <HeaderPanelComponent/>
-            )}
-            {gridState.data_collection && (
-                <>
-                    <FilterComponent/>
-                    <Table
-                        className="virtual-grid"
-                        rowSelection={getRowSelection()}
-                        dataSource={getDataSource()}
-                        columns={getColumnData()}
-                        scroll={{ x: 1500 }}
-                        pagination={{
-                            position: ["topRight", "bottomRight"],
-                            defaultPageSize: Number(urlParams.get('per-page')) ? Number(urlParams.get('per-page')) : 20,
-                            pageSizeOptions: ['20', '50', '100', '200'],
-                            showQuickJumper: true,
-                            hideOnSinglePage: true,
-                            showTotal: (total, range) => {
-                                return `Showing ${range[0]}-${range[1]} of ${total} items`;
-                            },
-                            current: gridState.data_collection.meta?.current_page,
-                            total: gridState.data_collection.meta?.total_count,
-                        }}
-                        onChange={onTableChange}
-                        // size={'small'}
-                    />
-                    <ColumnConfigComponent/>
-                </>
-            )}
+          <FilterComponent />
+          <Table
+            className="virtual-grid"
+            rowSelection={getRowSelection()}
+            dataSource={getDataSource()}
+            columns={getColumnData()}
+            scroll={{ x: 1500 }}
+            pagination={{
+              position: ['topRight', 'bottomRight'],
+              defaultPageSize: Number(urlParams.get('per-page'))
+                ? Number(urlParams.get('per-page'))
+                : 20,
+              pageSizeOptions: ['20', '50', '100', '200'],
+              showQuickJumper: true,
+              hideOnSinglePage: true,
+              showTotal: (total, range) => {
+                return `Showing ${range[0]}-${range[1]} of ${total} items`;
+              },
+              current: gridState.data_collection.meta?.current_page,
+              total: gridState.data_collection.meta?.total_count,
+            }}
+            onChange={onTableChange}
+            // size={'small'}
+          />
+          <ColumnConfigComponent />
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default TableComponent;
